4282fa778abb9342564e79235fc3534625b00fb8
Hossen <mdmitho199911@gmail.com>
2022-04-25 16:30:49 +0600

    Create JWT Token, Get jwt token on client side

--- src/Pages/Login/Login/Login.js
+++ src/Pages/Login/Login/Login.js
@@ -9,6 +9,7 @@ import SocialLogin from "../SocialLogin/SocialLogin";
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import PageTitle from "../../Shared/PageTitle/PageTitle";
+import axios from "axios";
 
 const Login = () =>{ 
     const emailRef= useRef('') 
@@ -31,7 +32,7 @@ const Login = () =>{
       return <Loading></Loading>
     }
 if(user){
-  navigate(from, {replace:true})
+  // navigate(from, {replace:true})
 }
 if (error) {
 
@@ -40,12 +41,16 @@ if (error) {
     </div>
 
 }
-const handleSubmit = event =>{
+const handleSubmit = async event =>{
     event.preventDefault()
     const email = emailRef.current.value
     const password = passwordRef.current.value
     // console.log(email, password);
-    signInWithEmailAndPassword(email, password)
+   await signInWithEmailAndPassword(email, password)
+   const {data }= await axios.post('http://localhost:5000/login',{email})
+  //  console.log(data);
+  localStorage.setItem('accessToken',data.accessToken)
+  navigate(from, {replace:true})
 }
 const navigateRegister = (event) =>{
     Navigate('/register')
