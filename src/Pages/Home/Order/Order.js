2fc94c4cad129ac6ea389feb9251202fc930f1e6
Hossen <mdmitho199911@gmail.com>
2022-04-25 15:03:17 +0600

    Filter orders info by email address Introduction JWT token

--- src/Pages/Home/Order/Order.js
+++ src/Pages/Home/Order/Order.js
@@ -1,9 +1,25 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+import {useAuthState} from 'react-firebase-hooks/auth'
+import auth from '../../../firebase.init'
 
 const Order = () => {
+    const [orders,setOrders] = useState([])
+    const [user] = useAuthState(auth)
+    
+useEffect(()=>{
+    const getOrders = async()=>{
+        const email = user.email
+        const url = `http://localhost:5000/order?email=${email}`
+        const {data} = await axios.get(url)
+        setOrders(data)
+    }
+    getOrders();
+},[])
+
     return (
         <div>
-            <h1>Your order</h1>
+            <h1>Your order : {orders.length}</h1>
         </div>
     );
 };
